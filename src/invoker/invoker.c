d6d26d37ffe0291c591046698d06a149248e031a
Alexey Shilov <alexey.shilov@nokia.com>
2010-02-26 13:33:00 +0200

    Changes: invoker clean-up

--- src/invoker/invoker.c
+++ src/invoker/invoker.c
@@ -1,21 +1,17 @@
 /*
- * Copyright © 2005, 2006, 2007, 2008, 2009, 2010 Nokia Corporation
+ * invoker.c
  *
- * Author: Guillem Jover <guillem.jover@nokia.com>
+ * This file is part of applauncherd
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
+ * Copyright (C) 2010 Nokia Corporation. All rights reserved.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
+ * This software, including documentation, is protected by copyright
+ * controlled by Nokia Corporation. All rights are reserved.
+ * Copying, including reproducing, storing, adapting or translating,
+ * any or all of this material requires the prior written consent of
+ * Nokia Corporation. This material also contains confidential
+ * information which may not be disclosed to others without the prior
+ * written consent of Nokia.
  */
 
 #define _GNU_SOURCE
@@ -45,60 +41,12 @@ enum APP_TYPE { DUI_APP, QT_APP, UNKNOWN_APP };
 
 extern char ** environ;
 
-//static pid_t invoked_pid = 0;
-
-//static void sig_forwarder(int sig)
-//{
-//    if (invoked_pid >= 0)
-//        kill(invoked_pid, sig);
-//}
-//
-//static void sigs_set(struct sigaction *sig)
-//{
-//    sigaction(SIGHUP, sig, NULL);
-//    sigaction(SIGINT, sig, NULL);
-//    sigaction(SIGQUIT, sig, NULL);
-//    sigaction(SIGTERM, sig, NULL);
-//    sigaction(SIGUSR1, sig, NULL);
-//    sigaction(SIGUSR2, sig, NULL);
-//    sigaction(SIGWINCH, sig, NULL);
-//}
-
-//static void sigs_init(void)
-//{
-//    struct sigaction sig;
-//
-//    memset(&sig, 0, sizeof(sig));
-//    sig.sa_flags = SA_RESTART;
-//    sig.sa_handler = sig_forwarder;
-//
-//    sigs_set(&sig);
-//}
-//
-//static void sigs_restore(void)
-//{
-//    struct sigaction sig;
-//
-//    memset(&sig, 0, sizeof(sig));
-//    sig.sa_flags = SA_RESTART;
-//    sig.sa_handler = SIG_DFL;
-//
-//    sigs_set(&sig);
-//}
-//
-//static void disable_self_core_dump(void)
-//{
-//    struct rlimit rlim = { 0, 0 };
-//
-//    if (setrlimit(RLIMIT_CORE, &rlim) != 0)
-//        warning("could not disable self core dumping");
-//}
 
 static bool invoke_recv_ack(int fd)
 {
     uint32_t action;
 
-    /* Revceive ACK. */
+    /* Receive ACK. */
     invoke_recv_msg(fd, &action);
 
     if (action != INVOKER_MSG_ACK)
@@ -127,10 +75,9 @@ static int invoker_init(enum APP_TYPE app_type)
     else
         die(1, "unknown type of application: %d\n", app_type);
 
-    debug("before connect()");
     if (connect(fd, (struct sockaddr *)&sun, sizeof(sun)) < 0)
         die(1, "connecting to the launcher\n");
-    debug("after connect()");
+
     return fd;
 }
 
@@ -178,59 +125,10 @@ static bool invoker_send_args(int fd, int argc, char **argv)
         debug("param %d %s \n", i, argv[i]);
         invoke_send_str(fd, argv[i]);
     }
-
     invoke_recv_ack(fd);
 
     return true;
 }
-
-//static bool invoker_send_prio(int fd, int prio)
-//{
-//    invoke_send_msg(fd, INVOKER_MSG_PRIO);
-//    invoke_send_msg(fd, prio);
-//
-//    invoke_recv_ack(fd);
-//
-//    return true;
-//}
-//
-//static bool invoker_send_io(int fd)
-//{
-//    struct msghdr msg;
-//    //struct msghdr msg = { 0 };
-//    struct cmsghdr *cmsg;
-//    int io[3] = { 0, 1, 2 };
-//    char buf[CMSG_SPACE(sizeof(io))];
-//    struct iovec iov;
-//    int dummy;
-//
-//    iov.iov_base = &dummy;
-//    iov.iov_len = 1;
-//
-//    msg.msg_iov = &iov;
-//    msg.msg_iovlen = 1;
-//    msg.msg_control = buf;
-//    msg.msg_controllen = sizeof(buf);
-//
-//    cmsg = CMSG_FIRSTHDR(&msg);
-//    cmsg->cmsg_len = CMSG_LEN(sizeof(io));
-//    cmsg->cmsg_level = SOL_SOCKET;
-//    cmsg->cmsg_type = SCM_RIGHTS;
-//
-//    memcpy(CMSG_DATA(cmsg), io, sizeof(io));
-//
-//    msg.msg_controllen = cmsg->cmsg_len;
-//
-//    invoke_send_msg(fd, INVOKER_MSG_IO);
-//    if (sendmsg(fd, &msg, 0) < 0)
-//    {
-//        warning("sendmsg failed in invoker_send_io: %s", strerror(errno));
-//        return  false;
-//    }
-//
-//    return true;
-//}
-
 
 static bool invoker_send_env(int fd)
 {
@@ -257,97 +155,6 @@ static bool invoker_send_end(int fd)
 
     return true;
 }
-//
-//static bool invoker_recv_pid(int fd)
-//{
-//    uint32_t action, pid;
-//
-//    // Receive action
-//    invoke_recv_msg(fd, &action);
-//
-//    if (action != INVOKER_MSG_PID)
-//    {
-//        die(1, "receiving bad pid (%08x)\n", action);
-//    }
-//
-//    // Receive pid
-//    invoke_recv_msg(fd, &pid);
-//    invoked_pid = pid;
-//
-//    return true;
-//}
-//
-//static int invoker_recv_exit(int fd)
-//{
-//    uint32_t action, status;
-//
-//    /* Receive action. */
-//    invoke_recv_msg(fd, &action);
-//
-//    if (action != INVOKER_MSG_EXIT)
-//    {
-//        die(1, "receiving bad exit status (%08x)\n", action);
-//    }
-//    /* Receive status. */
-//    invoke_recv_msg(fd, &status);
-//
-//    return status;
-//}
-//
-//static uint32_t  get_file_int(const char *filename)
-//{
-//    FILE *f;
-//    uint32_t u;
-//
-//    f = fopen(filename, "r");
-//    if (f == NULL)
-//        return 0;
-//
-//    if (fscanf(f, "%u", &u) < 1)
-//        u = 0;
-//
-//    fclose(f);
-//
-//    return u;
-//}
-
-///*
-// * This function is Linux specific and depends on the lowmem LSM kernel module,
-// * but should behave as a nop on systems not supporting it.
-// */
-//static unsigned int  get_linux_lowmem_modifier(void)
-//{
-//    uint32_t mem_allowed;
-//    uint32_t mem_used;
-//    const unsigned int lowmem_threshold = 80; /* Memory use percentage. */
-//    int modifier;
-//
-//    mem_allowed = get_file_int("/proc/sys/vm/lowmem_allowed_pages");
-//    mem_used = get_file_int("/proc/sys/vm/lowmem_used_pages");
-//
-//    if (mem_used && mem_allowed)
-//    {
-//        modifier = ((mem_used * 100) / mem_allowed) - lowmem_threshold;
-//        if (modifier < 0)
-//            modifier = 0;
-//    }
-//    else
-//        modifier = 0;
-//
-//    return modifier + 1;
-//}
-
-//static unsigned int get_delay(char *delay_arg)
-//{
-//    unsigned int delay;
-//
-//    if (delay_arg)
-//        delay = strtoul(delay_arg, NULL, 10);
-//    else
-//        delay = DEFAULT_DELAY;
-//
-//    return delay;
-//}
 
 static void version(void)
 {
@@ -379,8 +186,6 @@ int main(int argc, char *argv[])
     int prog_argc = 0;
     char **prog_argv = NULL;
     char *prog_name = NULL;
-//    int prog_prio = 0;
-    int prog_ret = 0;
     char *delay_str = NULL;
     int magic_options = 0;
     enum APP_TYPE app_type = UNKNOWN_APP;
@@ -422,7 +227,6 @@ int main(int argc, char *argv[])
             }
             else
             {
-
                 prog_name = search_program(argv[i]);
                 if (!prog_name)
                 {
@@ -447,11 +251,6 @@ int main(int argc, char *argv[])
     }
 
 
-//    errno = 0;
-//    prog_prio = getpriority(PRIO_PROCESS, 0);
-//    if (errno && prog_prio < 0)
-//        prog_prio = 0;
-
     info("invoking execution: '%s'\n", prog_name);
 
     fd = invoker_init(app_type);
@@ -460,35 +259,14 @@ int main(int argc, char *argv[])
     invoker_send_name(fd, prog_argv[0]);
     invoker_send_exec(fd, prog_name);
     invoker_send_args(fd, prog_argc, prog_argv);
-//    invoker_send_prio(fd, prog_prio);
-//    invoker_send_io(fd);
     invoker_send_env(fd);
     invoker_send_end(fd);
 
     if (prog_name)
         free(prog_name);
 
-//    int status;
-//
-    info("waiting for invoked program to exit\n");
-//
-//    invoker_recv_pid(fd);
-//
-//    /* We should disable self core dumps as late as possible, so that we have
-//         * a chance to dump on our own bugs. */
-//    disable_self_core_dump();
-//
-//    sigs_init();
-//    status = invoker_recv_exit(fd);
-//    sigs_restore();
-//
-//    if (WIFSIGNALED(status))
-//        raise(WTERMSIG(status));
-//    else if (WIFEXITED(status))
-//        prog_ret = WEXITSTATUS(status);
-
     close(fd);
 
-    return prog_ret;
+    return 0;
 }
 
