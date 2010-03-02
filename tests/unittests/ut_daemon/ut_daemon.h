a8f7868844981b927c5a384580f93eb7fa6c1c54
Jussi Lind <jussi.lind@nokia.com>
2010-03-02 14:38:47 +0200

    Changes: Useless project -commands removed in CMakeLists', fix compile problem with ut_daemon

---
+++ tests/unittests/ut_daemon/ut_daemon.h
@@ -0,0 +1,20 @@
+#ifndef UT_DAEMON_H
+#define UT_DAEMON_H
+
+#include<QtTest/QtTest>
+#include<QObject>
+
+class Ut_Daemon : public QObject
+{
+public:
+	Ut_Daemon();
+	~Ut_Daemon();
+public slots:
+
+private:
+
+};
+
+#endif // UT_DAEMON_H
+
+
