1c5ca25d366da0a841af04708f0821c4a5e6fad2
Antti Kervinen <antti.kervinen@nokia.com>
2010-03-18 16:43:32 +0200

    Changes: test code LGPL'd as well
    RevBy: TrustMe

--- tests/functests/test-func-launcher.py
+++ tests/functests/test-func-launcher.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+#
+# test-func-launcher.py
+#
+# This file is part of applauncherd
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
 """
 This program tests the startup time of the given application with and
 without launcher.
@@ -16,6 +38,7 @@ Example:  test-func-launcher /usr/bin/helloworld
 
 Author:   ext-nimika.1.keshri@nokia.com
 """
+
 import os
 import subprocess
 import commands
--- tests/unittests/ut_booster/ut_booster.h
+++ tests/unittests/ut_booster/ut_booster.h
@@ -3,15 +3,22 @@
  *
  * This file is part of applauncherd
  *
- * Copyright (C) 2010 Nokia Corporation. All rights reserved.
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
  *
- * This software, including documentation, is protected by copyright
- * controlled by Nokia Corporation. All rights are reserved.
- * Copying, including reproducing, storing, adapting or translating,
- * any or all of this material requires the prior written consent of
- * Nokia Corporation. This material also contains confidential
- * information which may not be disclosed to others without the prior
- * written consent of Nokia.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 
 #ifndef UT_BOOSTER_H
