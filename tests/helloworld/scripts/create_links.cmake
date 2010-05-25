11d82f2923f3598edfe6c73a342e25ce32d487a4
Nimika Keshri <nkeshri@Nimika.nmp.nokia.com>
2010-05-25 17:05:22 +0300

    Changes: symlinks for functional tests renamed as well
    RevBy: TrustMe

--- tests/helloworld/scripts/create_links.cmake
+++ tests/helloworld/scripts/create_links.cmake
@@ -1,6 +1,6 @@
-execute_process(COMMAND echo "Creating symlinks for helloworld..")
-execute_process(COMMAND ln -v -s /usr/bin/helloworld $ENV{DESTDIR}/usr/bin/helloworld_1)
-execute_process(COMMAND ln -v -s /usr/bin/helloworld $ENV{DESTDIR}/usr/bin/helloworld_2)
-execute_process(COMMAND ln -v -s /usr/bin/helloworld.launch $ENV{DESTDIR}/usr/bin/helloworld_1.launch)
-execute_process(COMMAND ln -v -s /usr/bin/helloworld.launch $ENV{DESTDIR}/usr/bin/helloworld_2.launch)
+execute_process(COMMAND echo "Creating symlinks for fala_ft_hello..")
+execute_process(COMMAND ln -v -s /usr/bin/fala_ft_hello $ENV{DESTDIR}/usr/bin/fala_ft_hello_1)
+execute_process(COMMAND ln -v -s /usr/bin/fala_ft_hello $ENV{DESTDIR}/usr/bin/fala_ft_hello_2)
+execute_process(COMMAND ln -v -s /usr/bin/fala_ft_hello.launch $ENV{DESTDIR}/usr/bin/fala_ft_hello_1.launch)
+execute_process(COMMAND ln -v -s /usr/bin/fala_ft_hello.launch $ENV{DESTDIR}/usr/bin/fala_ft_hello_2.launch)
 
