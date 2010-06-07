71991e3de11387b979147d6ec7f376ea8fa93211
Nimika Keshri <nimika.1.keshri@nokia.com>
2010-06-07 18:08:44 +0300

    Changes: New directory for testapps craeted RevBy: TrustMe

---
+++ tests/TestApps/testapp/main.cpp
@@ -0,0 +1,62 @@
+#include <MApplication>
+#include <MApplicationWindow>
+#include <MApplicationPage>
+
+#ifdef HAVE_MCOMPONENTCACHE   
+    #include <MComponentCache>
+#endif
+
+#include <MExport>
+#include <QFile>
+#include <sys/time.h>
+
+void FANGORNLOG(const char* s)
+{
+    QFile f("/tmp/testapp.log");
+    f.open(QIODevice::Append);
+    f.write(s, qstrlen(s));
+    f.write("\n", 1);
+    f.close();
+}
+
+void timestamp(const char *s)
+{
+    timeval tim;
+    char msg[80];
+    gettimeofday(&tim, NULL);
+    snprintf(msg, 80, "%d.%06d %s\n", 
+             static_cast<int>(tim.tv_sec), static_cast<int>(tim.tv_usec), s);
+    FANGORNLOG(msg);
+}
+
+class MyApplicationPage: public MApplicationPage
+{
+public:
+    MyApplicationPage(): MApplicationPage() {}
+    virtual ~MyApplicationPage() {}
+    void enterDisplayEvent() {
+        timestamp("MyApplicationPage::enterDisplayEvent");
+    }
+};
+
+M_EXPORT int main(int, char**);
+
+int main(int argc, char **argv) {
+    timestamp("application main");
+    
+#ifdef HAVE_MCOMPONENTCACHE   
+    MApplication* app = MComponentCache::mApplication(argc, argv);
+    MApplicationWindow* w = MComponentCache::mApplicationWindow();
+#else
+    MApplication* app = new MApplication(argc, argv);
+    MApplicationWindow* w = new MApplicationWindow;
+#endif
+    
+    MyApplicationPage p;
+    MApplication::setPrestartMode(M::LazyShutdown);
+    p.setTitle("Applauncherd testapp");
+    w->show();
+    p.appear();
+    return app->exec();
+}
+
